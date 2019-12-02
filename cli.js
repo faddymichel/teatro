0ae2934864280ed68c91ed180916469bd9c2f68c
Faddy Michel <faddy@pseudomode.pseudomode.com>
2019-12-02 08:01:11 -0500

    @faddymichel: #bin

--- cli.js
+++ cli.js
@@ -2,6 +2,44 @@
 
 'use strict';
 
+const argv = require ( 'yargs' )
+.option ( 'host', {
+
+alias: 'H',
+group: 'Address:',
+description: 'The host on which the server will listen for new connections.',
+default: 'localhost',
+type: 'string'
+
+} )
+.option ( 'port', {
+
+alias: 'P',
+group: 'Address:',
+description: 'The port on which the server will listen for new connections.',
+default: 1313,
+type: 'number'
+
+} )
+.argv;
+
+const teatro = require ( './' ) .open ( {
+
+host: argv .host,
+port: argv .port
+
+} );
+
+console .log ( 'Teatro:', 'Opening ...', teatro );
+
+teatro .on ( 'open', ( ticket ) => {
+
+console .log ( 'Teatro:', 'open ...', ticket );
+
+} );
+
+/* old
+
 const server = require ( 'server' );
 
 process .on ( 'SIGINT', ()=> {
@@ -21,3 +59,5 @@ console .log ( 'killed Pass:', pass .pid, '...' );
 } );
 
 } );
+
+*/
