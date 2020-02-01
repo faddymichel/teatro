24bdf4f9b98f5c3723788c4eac9d5ddc25891b02
Faddy Michel <faddy@pseudomode.pseudomode.com>
2020-02-01 18:35:35 -0500

    @faddymichel: #play #end #participant

--- module/play.js
+++ module/play.js
@@ -6,6 +6,12 @@ descriptor .value = function play ( participant, ticket ) {
 
 const teatro = this;
 
+venue [ ticket .play ] .on ( 'end', () => {
+
+participant .end ( '#play #end' );
+
+} );
+
 venue [ ticket .play ] .scenario .call ( teatro, participant );
 
 };
