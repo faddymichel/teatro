3dfd4c63d27352d3b11345f7d64f9226652d6e36
Faddy Michel <faddy@local.teatro13.com>
2020-02-10 21:17:42 -0500

    @faddymichel: #repository #separation #module #cli

---
+++ examples/yallah/scenario.js
@@ -0,0 +1,44 @@
+module .exports = function scenario$yallah ( participant ) {
+
+const teatro = this;
+const interface = require ( 'readline' ) .createInterface ( {
+
+input: participant,
+output: participant,
+prompt: 'Say Yallah!'
+
+} );
+
+participant .on ( 'error', () => {
+
+interface .removeAllListeners ();
+interface .close ();
+
+} );
+
+interface .prompt ();
+interface .on ( 'line', ( line ) => {
+
+line = line
+.trim ()
+.toLowerCase ();
+
+switch ( line ) {
+
+case 'yallah':
+
+participant .write ( 'Salah Abdallah!' );
+
+break;
+
+default:
+
+participant .write ( 'I said YALLAH!' );
+
+}
+
+interface .prompt ();
+
+} );
+
+};
---
+++ examples/yallah/teatro.js
@@ -0,0 +1,52 @@
+const Teatro = require ( '@teatro13/teatro' );
+const teatro = new Teatro ();
+
+teatro .on ( 'open', () => {
+
+teatro .on ( 'participant', ( participant ) => {
+
+participant .setEncoding ( 'utf8' );
+participant .on ( 'data', ( stamp ) => {
+
+console .log ( 'incoming stamp:', stamp );
+
+stamp = stamp .trim ();
+
+const ticket = teatro .retrieve ( stamp );
+
+if ( ticket ) {
+
+participant .removeAllListeners ();
+teatro .play ( participant, ticket );
+
+} else
+
+participant .end ( 'Get out! Actually, you are out!' );
+
+} );
+
+participant .write ( 'Show me the Stamp of your Ticket. Or, get out of here!' );
+
+} );
+
+const scenario = require ( './scenario' );
+const signature = Symbol ();
+const play = teatro .host ( scenario, signature );
+const stamp = teatro .issue ( play );
+
+console .log ( 'The Play "Yallah" is now hosted on Teatro.' );
+console .log ( 'Here is the stamp for the issued ticket. They will not let you in without it; they are taking it serious for some reason.' );
+console .log ( 'Stamp:', stamp );
+
+} );
+
+const options = {};
+options .ws = {
+
+host: 'localhost',
+port: 1313
+
+};
+const key = options .lock = Symbol ();
+
+teatro .open ( options );
