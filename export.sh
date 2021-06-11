b048619e3f9e3b205b2cd756d370feaa0250cfeb
Faddy Michel <faddy.michel@gmail.com>
2021-06-11 09:20:21 -0400

    @faddymichel: #scenarist #clear

--- export.sh
+++
@@ -1,7 +0,0 @@
-cat script.js | tee index.js index.mjs > /dev/null
-
-echo '' >> index.js
-echo 'module .exports = Scenarist;' >> index.js
-
-echo '' >> index.mjs
-echo 'export default Scenarist;' >> index.mjs
